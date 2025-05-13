408b4389a4be2a460d73c9986b0a6e71fdb22961
flipflip <144994269+flipflip8952@users.noreply.github.com>
2025-05-13 14:15:32 +0200

    cosmetics (#112)

--- fpsdk_apps/parsertool/parsertool_doc.hpp
+++ fpsdk_apps/parsertool/parsertool_doc.hpp
@@ -38,6 +38,8 @@ namespace parsertool {
 
     @section FPSDK_APPS_PARSERTOOL_EXAMPLES Examples
 
+    <b>Print info on all messages found in a file:</b>
+
     @verbatim
     $ parsertool fpsdk_common/test/data/mixed.bin
     @endverbatim
@@ -86,6 +88,8 @@ namespace parsertool {
     OTHER             8 ( 29.6%)        962 ( 33.9%)
     @endverbatim
 
+
+    <b>Print info on selected messages found in a file:</b>
 
     @verbatim
     $ parsertool -f NMEA,FP_A fpsdk_common/test/data/mixed.bin
